e699deeda71bb0d7b94a512e9d357872e6464c36
rwhs <ronald.so1996@gmail.com>
2020-12-17 21:31:10 -0500

    set up typescript

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,12 @@
-console.log('test')
+import express from 'express';
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Testing express route');
+    console.log('GET request on \'/\'')
+})
+
+app.listen(port, () => {
+    console.log(`Express server started on port ${port}. Listening...`)
+})
